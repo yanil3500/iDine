5663177e9c59ad6f4885a66beb122ca1fb7351c2
Elyanil <yanil3500@gmail.com>
2019-10-04 21:00:25 -0400

    initial commit

---
+++ iDine/iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Elyanil Liranzo Castro on 10/4/19.
+//  Copyright © 2019 Elyanil Liranzo Castro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
